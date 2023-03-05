217611557388179bb35b1fbf6bb61499a9ed34be
GHOST <ghostdevbusiness@gmail.com>
2023-03-05 00:01:29 +0000

    feat: $lib alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
     plugins: [svelte()],
+
+    server: {
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin',
+        },
+    },
+
+    resolve: {
+        alias: {
+            $lib: resolve('./src/lib'),
+        },
+    },
 });
