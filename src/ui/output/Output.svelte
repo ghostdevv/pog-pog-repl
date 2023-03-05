28d88d7730b67983ea1248ee5ad72fd01c8be840
GHOST <ghostdevbusiness@gmail.com>
2023-03-05 22:46:05 +0000

    feat: refresh and url bar

--- src/ui/output/Output.svelte
+++ src/ui/output/Output.svelte
@@ -1,26 +1,60 @@
 <script lang="ts">
+    import { faRefresh } from '@fortawesome/free-solid-svg-icons';
     import { get_container } from '$lib/container';
+    import Fa from 'svelte-fa';
 
     const container = get_container();
 
-    let src: string | undefined = undefined;
+    let src: string = '';
 
     container.on('server-ready', (_port: number, url: string) => {
         src = url;
     });
+
+    function reload() {
+        src = src;
+    }
 </script>
 
-<iframe class="output" {src} title="Result" />
+<div class="output">
+    <div class="controls">
+        <button class="icon" on:click={reload}>
+            <Fa icon={faRefresh} />
+        </button>
+
+        <input type="text" class="url" bind:value={src} />
+    </div>
+
+    <iframe class="frame" {src} title="Result" />
+</div>
 
 <style lang="scss">
     .output {
         grid-area: output;
 
-        width: 100%;
-        height: 100%;
-        max-height: 100%;
+        display: flex;
+        flex-direction: column;
 
-        border: none;
-        background-color: #eee;
+        .controls {
+            background-color: var(--background-secondary);
+            padding: 12px 8px;
+
+            display: flex;
+            align-items: center;
+            gap: 8px;
+        }
+
+        .url {
+            padding: 8px 12px;
+            border-width: 2px;
+            font-size: 1rem;
+        }
+
+        .frame {
+            flex-grow: 1;
+
+            border: none;
+            background-color: #eee;
+        }
     }
 </style>
