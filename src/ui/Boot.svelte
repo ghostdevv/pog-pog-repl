95566392cd7b434f2ab57b6b00d64730e12a3729
GHOST <ghostdevbusiness@gmail.com>
2023-03-05 01:03:01 +0000

    feat: booter

--- src/ui/Boot.svelte
+++ src/ui/Boot.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+    import { WebContainer } from '@webcontainer/api';
     import Root from './Root.svelte';
+
+    let container = new Promise<WebContainer>(() => {});
+
+    window.addEventListener('load', () => {
+        container = WebContainer.boot();
+    });
 </script>
 
-<Root />
+{#await container}
+    LOADING
+{:then container}
+    <Root {container} />
+{:catch error}
+    Error {error}
+{/await}
